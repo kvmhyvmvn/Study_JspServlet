cf9829a597fb6818a915099ed6880d925e042114
kvmhyvmvn <97383799+kvmhyvmvn@users.noreply.github.com>
2023-05-22 12:55:59 +0900

    Request, Scope

---
+++ WorkSpace/04.Implicit/src/main/java/Controller/Ex02_Request.java
@@ -0,0 +1,28 @@
+package Controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Ex02_Request")
+public class Ex02_Request extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		for(int i=1; i<=30; i++) {
+			System.out.println(request.getParameter("name"+i));
+		}
+		System.out.println("GET 실행");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
